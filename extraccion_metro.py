973f11b8f60f548ce19a13d4be88dd2f74f3bed9
Vicente Pardo Silva <vicente.pardo@estudiante.uc.cl>
2025-10-12 16:21:50 -0300

    version inicial

---
+++ extraccion_metro.py
@@ -0,0 +1,40 @@
+# Creamos un dataframe que tenga todos los ID de paraderos de Santiago usando
+# https://raw.githubusercontent.com/JoseDTPM/geojson-Transantiago/refs/heads/main/Paraderos-Santiago-Chile.geojsonl.json
+
+import pandas as pd
+import requests
+import time
+from datetime import datetime
+from alive_progress import alive_bar
+
+def get_metro_data():
+    url = "https://api.xor.cl/red/metro-network"
+    all_stations_data = []
+
+    response = requests.get(url)
+    lines_data = response.json()
+
+    lines_list = lines_data.get("lines", [])
+
+    for line in lines_list:
+        line_id = line.get("id")
+        
+        stations = line.get("stations", [])
+        
+        for station in stations:
+            all_stations_data.append({
+                "line_id": line_id,
+                "station_id": station.get("id"),
+                "name": station.get("name"),
+                "status_code": station.get("status"),
+                "status_description": station.get("description")
+            })
+    
+    return pd.DataFrame(all_stations_data)
+
+timeanddate = datetime.now()
+timeanddate = timeanddate.strftime("%d-%m-%Y-%H:%M")
+    
+metro_df = get_metro_data()
+
+metro_df.to_csv(f"metro_{timeanddate}.csv", index=False)
