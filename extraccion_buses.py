348772570cacd6b8ae166b063669c1ef15548cb6
Vicente Pardo Silva <vicente.pardo@estudiante.uc.cl>
2025-10-12 10:03:00 -0300

    extraccion_buses.py versión 1 añadido

---
+++ extraccion_buses.py
@@ -0,0 +1,66 @@
+# Creamos un dataframe que tenga todos los ID de paraderos de Santiago usando
+# https://raw.githubusercontent.com/JoseDTPM/geojson-Transantiago/refs/heads/main/Paraderos-Santiago-Chile.geojsonl.json
+
+import pandas as pd
+import requests
+import time
+from datetime import datetime
+from alive_progress import alive_bar
+
+file = 'data/Paraderos-Santiago-Chile.geojsonl.json'
+df = pd.read_json(file, lines=True)
+properties_df = pd.json_normalize(df['properties'])
+
+# Seleccionamos la columna que corresponde al ID de los paraderos.
+stops_df = properties_df[['SIMT', 'X', 'Y']]
+stops_df_clean = pd.DataFrame()
+stops_df_clean['SIMT'] = simt_df['SIMT'].unique()
+print(f"cant. normal: {len(stops_df)}\ncant. limpio: {len(stops_df_clean)}")
+
+timeanddate = datetime.now()
+
+timeanddate = timeanddate.strftime("%d-%m-%Y-%H:%M:%S")
+
+def bus_routes(stop):
+    all_routes_data = []
+    total_stops = len(stop)
+
+    print(f"\ntotal {total_stops} paradas")
+    i = 0
+    # añadir código de la barra de progreso
+    for code in stops_df['SIMT']:
+        if i > 10:
+            break
+        i += 1
+        print(f"{i}: {code}")
+        url = f"https://api.xor.cl/red/bus-stop/{code}"
+        response = requests.get(url)
+        data = response.json()
+        
+        for service in data.get('services', []):
+                for bus in service.get('buses', []):
+                    all_routes_data.append({
+                        'bus_stop_code': code,
+                        'route_id': service.get('id'),
+                        'bus_id': bus.get('id'),
+                        'meters_distance': bus.get('meters_distance'),
+                        'min_arrival_time': bus.get('min_arrival_time'),
+                        'max_arrival_time': bus.get('max_arrival_time')
+            })
+    bus_routes = pd.DataFrame(all_routes_data)                
+    final_df = pd.merge(
+    bus_routes,
+    stops_df,
+    left_on='bus_stop_code',
+    right_on='SIMT',
+    how='left'
+    )
+
+    final_df = final_df.drop(columns=['SIMT'])
+    return final_df
+
+sample_codes = stops_df
+
+final_bus_df = bus_routes(sample_codes)
+
+final_bus_df.to_csv(f"datos_{timeanddate}.csv", index=False)
