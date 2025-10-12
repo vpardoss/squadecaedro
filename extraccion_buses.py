5464136da9f2b8fe5bbed61d3e587adde654b68b
Vicente Pardo Silva <vicente.pardo@estudiante.uc.cl>
2025-10-12 16:59:06 -0300

    cambiado output a nuevo directorio

--- extraccion_buses.py
+++ extraccion_buses.py
@@ -7,55 +7,57 @@ import time
 from datetime import datetime
 from alive_progress import alive_bar
 
-file = 'data/Paraderos-Santiago-Chile.geojsonl.json'
-df = pd.read_json(file, lines=True)
-properties_df = pd.json_normalize(df['properties'])
-
-# Seleccionamos la columna que corresponde al ID de los paraderos.
-stops_df = properties_df[['SIMT', 'X', 'Y']]
-stops_df_clean = pd.DataFrame()
-stops_df_clean['SIMT'] = stops_df['SIMT'].unique()
-stops_df_500 = stops_df.sample(n = 500).reset_index()
-print(f"Comenzando extracción:")
-timeanddate = datetime.now()
-timeanddate = timeanddate.strftime("%d-%m-%Y-%H:%M")
-
 def bus_routes(stop):
     all_routes_data = []
     total_stops = len(stop)
-    i = 0
     with alive_bar(total_stops) as bar:
-        for code in stop['SIMT']:
+        for code in stop["SIMT"]:
             print(f"Paradero actual: {code}")
             url = f"https://api.xor.cl/red/bus-stop/{code}"
             response = requests.get(url)
             data = response.json()
             
-            for service in data.get('services', []):
-                    for bus in service.get('buses', []):
+            for service in data.get("services", []):
+                    for bus in service.get("buses", []):
+                        current_datetime = datetime.now()
                         all_routes_data.append({
-                            'bus_stop_code': code,
-                            'route_id': service.get('id'),
-                            'bus_id': bus.get('id'),
-                            'meters_distance': bus.get('meters_distance'),
-                            'min_arrival_time': bus.get('min_arrival_time'),
-                            'max_arrival_time': bus.get('max_arrival_time')
+                            "bus_stop_code": code,
+                            "route_id": service.get("id"),
+                            "bus_id": bus.get("id"),
+                            "meters_distance": bus.get("meters_distance"),
+                            "min_arrival_time": bus.get("min_arrival_time"),
+                            "max_arrival_time": bus.get("max_arrival_time"),
+                            "date": current_datetime
                 })
             bar()
     bus_routes = pd.DataFrame(all_routes_data)
     final_df = pd.merge(
     bus_routes,
     stops_df,
-    left_on='bus_stop_code',
-    right_on='SIMT',
-    how='left'
+    left_on="bus_stop_code",
+    right_on="SIMT",
+    how="left"
     )
 
-    final_df = final_df.drop(columns=['SIMT'])
+    final_df = final_df.drop(columns=["SIMT"])
     return final_df
+
+
+file = "data/Paraderos-Santiago-Chile.geojsonl.json"
+df = pd.read_json(file, lines=True)
+properties_df = pd.json_normalize(df["properties"])
+
+# Seleccionamos la columna que corresponde al ID de los paraderos y sus coordenadas.
+stops_df = properties_df[["SIMT", "X", "Y"]]
+stops_df_clean = pd.DataFrame()
+stops_df_clean["SIMT"] = stops_df["SIMT"].unique()
+stops_df_500 = stops_df.sample(n = 500).reset_index()
+timeanddate = datetime.now()
+timeanddate = timeanddate.strftime("%d-%m-%Y-%H:%M")
+
 
 final_bus_df = bus_routes(stops_df_500)
 final_bus_df
 
-final_bus_df.to_csv(f"datos_{timeanddate}.csv", index=False)
+final_bus_df.to_csv(f"buses_outputs/datos_{timeanddate}.csv", index=False)
 print("CSV creado.")
