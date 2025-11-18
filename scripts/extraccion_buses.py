fcdae288abcc54b30ebd2902e17a6af75acc2eba
Vicente Pardo Silva <vicente.pardo@estudiante.uc.cl>
2025-11-18 15:02:26 -0300

    movidos archivos a carpetas por orden

---
+++ scripts/extraccion_buses.py
@@ -0,0 +1,66 @@
+# Creamos un dataframe que tenga todos los ID de paraderos de Santiago usando
+# https://raw.githubusercontent.com/JoseDTPM/geojson-Transantiago/refs/heads/main/Paraderos-Santiago-Chile.geojsonl.json
+
+import pandas as pd
+import requests
+import time
+from datetime import datetime
+from alive_progress import alive_bar
+
+def bus_routes(stop):
+    all_routes_data = []
+    total_stops = len(stop)
+    with alive_bar(total_stops) as bar:
+        for code in stop["SIMT"]:
+            print(f"Paradero actual: {code}")
+            url = f"https://api.xor.cl/red/bus-stop/{code}"
+            try:
+                response = requests.get(url)
+            except:
+                break
+            data = response.json()
+            
+            for service in data.get("services", []):
+                    for bus in service.get("buses", []):
+                        current_datetime = datetime.now()
+                        all_routes_data.append({
+                            "bus_stop_code": code,
+                            "route_id": service.get("id"),
+                            "bus_id": bus.get("id"),
+                            "meters_distance": bus.get("meters_distance"),
+                            "min_arrival_time": bus.get("min_arrival_time"),
+                            "max_arrival_time": bus.get("max_arrival_time"),
+                            "date": current_datetime
+                })
+            bar()
+    bus_routes = pd.DataFrame(all_routes_data)
+    final_df = pd.merge(
+    bus_routes,
+    stops_df,
+    left_on="bus_stop_code",
+    right_on="SIMT",
+    how="left"
+    )
+
+    final_df = final_df.drop(columns=["SIMT"])
+    return final_df
+
+
+file = "data/Paraderos-Santiago-Chile.geojsonl.json"
+df = pd.read_json(file, lines=True)
+properties_df = pd.json_normalize(df["properties"])
+
+# Seleccionamos la columna que corresponde al ID de los paraderos y sus coordenadas.
+stops_df = properties_df[["SIMT", "X", "Y"]]
+stops_df_clean = pd.DataFrame()
+stops_df_clean["SIMT"] = stops_df["SIMT"].unique()
+stops_df_1000 = stops_df.sample(n = 1000).reset_index()
+timeanddate = datetime.now()
+timeanddate = timeanddate.strftime("%d-%m-%Y-%H-%M")
+
+
+final_bus_df = bus_routes(stops_df_1000)
+final_bus_df
+
+final_bus_df.to_csv(f"buses_outputs/datos_{timeanddate}.csv", index=False)
+print("CSV creado.")
